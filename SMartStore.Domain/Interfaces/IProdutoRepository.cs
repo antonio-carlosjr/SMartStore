ee8734e0f736292a3c544800f9b3b3917dabb0ee
Antonio Carlos <profissional.contatocarlos@gmail.com>
2025-05-06 12:16:32 -0300

    Criação da interface IProdutoRepository

---
+++ SMartStore.Domain/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SMartStore.Domain.Entities;
+
+namespace SMartStore.Domain.Interfaces
+{
+    internal interface IProdutoRepository
+    {
+        Produto obterPorId(int id);
+        IEnumerable<Produto> Listar();
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+        void Remover(int id);
+
+    }
+}
