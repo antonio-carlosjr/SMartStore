06b4b86bfdf372a5776e0421571e20d17bfa0177
Antonio Carlos <profissional.contatocarlos@gmail.com>
2025-05-06 12:22:09 -0300

    Criação de DTO ProdutoDTO

---
+++ SMartStore.Application/DTOs/ProdutoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMartStore.Application.DTOs
+{
+    public class ProdutoDTO
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Tipo { get; set; } // "fisico" ou "digital"
+        public decimal? Peso { get; set; } // apenas para produtos físicos
+    }
+}
