95962197f4a1b1cb73bd64b24f9453b6a4ae7295
Antonio Carlos <profissional.contatocarlos@gmail.com>
2025-05-06 12:00:31 -0300

    Primeira implementação das Entities da camada Domain

---
+++ SMartStore.Domain/Entities/ProdutoDigital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMartStore.Domain.Entities
+{
+    public class ProdutoDigital : Produto
+    {
+        public override decimal CalcularDesconto()
+        {
+             return Preco * 0.3m; // 30% de desconto     
+        }
+    }
+}
---
+++ SMartStore.Domain/Entities/ProdutoFisico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMartStore.Domain.Entities
+{
+    public class ProdutoFisico : Produto
+    {
+        public decimal Peso { get; set; }
+
+        public override decimal CalcularDesconto()
+        {
+            return Preco * 0.1m; 
+        }
+    }
+}
